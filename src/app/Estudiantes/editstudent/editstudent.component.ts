6671cbe0ef31dc6982abb54ed57beb0e2b364d30
Javier Dávila <jav98dav@gmail.com>
2022-09-19 23:33:04 -0600

    Consumo de servicios rest para edicion de estudiante

--- src/app/Estudiantes/editstudent/editstudent.component.ts
+++ src/app/Estudiantes/editstudent/editstudent.component.ts
@@ -11,12 +11,18 @@ declare var $:any;
   styleUrls: ['./editstudent.component.scss']
 })
 export class EditstudentComponent implements OnInit {
-  public em: any;
+  public em: StudentModel;
+  public p: PersonaModel;
+  public f: string;
 
   constructor(private rutaActiva: ActivatedRoute,
               private estudianteService: EstudianteService,
               private rout: Router) {
 
+
+  }
+
+  ngOnInit(): void {
     let idCui = this.rutaActiva.snapshot.paramMap.get('id');
     try{
       this.estudianteService.getEstudiantesDatosInd(+idCui).subscribe(result=>{
@@ -39,14 +45,11 @@ export class EditstudentComponent implements OnInit {
           monthstring = month;
         }
         let yearstring = year;
-        this.em.fecha = `${year}-${month}-${day}`;
+        this.f = `${year}-${month}-${day}`;
       })
     }catch (e){
       console.log(`Error: ${e}`)
     }
-  }
-
-  ngOnInit(): void {
   }
 
   submit(formStudent: any, student: any){
@@ -56,8 +59,20 @@ export class EditstudentComponent implements OnInit {
       let mm: number = student.fecha.toString().slice(5, -3)-1;
       let dd: number = +student.fecha.toString().slice(-2);
       console.log(`Año ${aaaa}, Mes ${mm}, Dia ${dd}`);
-      student.nacimiento = new Date(aaaa,mm,dd);
+      this.p.nacimiento = new Date(aaaa,mm,dd);
+      this.p.cui = student.cui;
+      this.p.nombre = student.nombre;
+      this.p.apellido = student.apellido;
+      this.p.email = student.email;
+      this.p.telefono = student.telefono;
+      this.p.direccion = student.direccion;
+      this.p.studentList = student.studentList;
+      this.p.profesorList = student.profesorList;
       //this.estudianteService.editEstudiante(student);
+      /*this.estudianteService.putEditPersona(this.p).subscribe((result)=>{
+
+      });*/
+      alert(JSON.stringify(this.p));
       this.showNotification('top','right', 2, 'pe-7s-check',"Estudiante actualizado");
     }else{
       this.showNotification('top','right', 4, 'pe-7s-close-circle',"Complete todos los campos");
